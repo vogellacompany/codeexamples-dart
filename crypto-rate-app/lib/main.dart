0743ff30ac86655d0c32c454967e527a04898455
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2018-03-21 16:59:45 +0100

    [Crypto] Fixed some minor issues.

--- crypto-rate-app/lib/main.dart
+++ crypto-rate-app/lib/main.dart
@@ -38,26 +38,24 @@ class OverviewState extends State<Overview> {
   /// whicht refreshes and rebuilds the list that is shown to the user, 
   /// 
   listenForEntries() async{
-    _map.putIfAbsent("BTC", () => new CryptoEntry("Bitcoin", "BTC", 0.0));
-    _map.putIfAbsent("BTC", () => new CryptoEntry("Bitcoin Cash", "BCH", 0.0));
-    _map.putIfAbsent("BTC", () => new CryptoEntry("Litecoin", "LTC", 0.0));
-    _map.putIfAbsent("BTC", () => new CryptoEntry("Ripple", "XRP", 0.0));
-  	_map.putIfAbsent("BTC", () => new CryptoEntry("Etherum", "ETH", 0.0));
+    _map["BTC"] = new CryptoEntry("Bitcoin", "BTC", -1.0);
+    _map["BCH"] = new CryptoEntry("Bitcoin Cash", "BCH", -1.0);
+    _map["LTC"] = new CryptoEntry("Litecoin", "LTC", -1.0);
+    _map["XRP"] = new CryptoEntry("Ripple", "XRP", -1.0);
+  	_map["ETH"] = new CryptoEntry("Etherum", "ETH", -1.0);
     var url = "https://min-api.cryptocompare.com/data/pricemulti?tsyms=EUR&fsyms=BTC,BCH,LTC,XRP,ETH";
     var httpClient = new HttpClient();
-    Map data;
     try{
       var request = await httpClient.getUrl(Uri.parse(url));
       var response = await request.close();
       if(response.statusCode == HttpStatus.OK) {
         var json = await response.transform(UTF8.decoder).join();
-        data = JSON.decode(json);
+        Map data = JSON.decode(json);
         data.forEach((e, x) {
           double conversionRate = x["EUR"];
           CryptoEntry entry = _map[e];
-          setState(() =>
-            entry.conversionRate = conversionRate
-          );
+          if(entry == null) return;
+          setState(() => entry.conversionRate = conversionRate);
         });
       }
     }catch(exception) {
@@ -80,14 +78,11 @@ class OverviewState extends State<Overview> {
             ),
           ]
         ),
-        body: new RefreshIndicator(
-          child: new Center(
-            child: new ListView(
-              children: _map.values.map((entry) => new CryptoWidget(entry)).toList(),
-            )
-          ),
-          onRefresh: _refresh(),
-        )
+        body: new Center(
+          child: new ListView(
+            children: _map.values.map((entry) => new CryptoWidget(entry)).toList(),
+          )
+        ),
       ),
     );
   }
@@ -127,17 +122,17 @@ class CryptoWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Card(
-      elevation: 1.0,
+      elevation: 0.0,
       child: new Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           new ListTile(
             leading: new CircleAvatar(
-              child: new Text(_cryptoEntry.titleAbr),
+              child: new Text(_cryptoEntry.abreviation),
             ),
-            title: new Text(_cryptoEntry.titleAbr),
-            subtitle: new Text(_cryptoEntry.titleFull),
-            trailing: new Text("${_cryptoEntry.conversionRate}€", textScaleFactor: 2.0,),
+            title: new Text(_cryptoEntry.abreviation),
+            subtitle: new Text(_cryptoEntry.name),
+            trailing: new Text("${(_cryptoEntry.conversionRate == -1.0 ? '-' : _cryptoEntry.conversionRate)}€", textScaleFactor: 1.5,),
           ),
         ]
       ),
